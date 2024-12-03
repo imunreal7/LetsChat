c0bcfa7fe7544e0107d200336cd20f057de9b9da
aman.dubey <aman.dubey@atmecs.com>
2024-12-03 13:15:45 +0530

    Solved the bug of name

--- client/src/components/ChatHeader.jsx
+++ client/src/components/ChatHeader.jsx
@@ -15,14 +15,14 @@ const ChatHeader = () => {
                         <div className="size-10 rounded-full relative">
                             <img
                                 src={selectedUser.profilePic || "/avatar.png"}
-                                alt={selectedUser.fullName}
+                                alt={selectedUser.name}
                             />
                         </div>
                     </div>
 
                     {/* User info */}
                     <div>
-                        <h3 className="font-medium">{selectedUser.fullName}</h3>
+                        <h3 className="font-medium">{selectedUser.name}</h3>
                         <p className="text-sm text-base-content/70">
                             {onlineUsers.includes(selectedUser._id) ? "Online" : "Offline"}
                         </p>
